d420bcbea3d868d9f5d4f270d77702f5e50b45ee
nor <norapat.sm@gmail.com>
2023-07-01 17:12:55 +0700

    น่าจะเสร็จมั้ง

--- class_work1_v3.py
+++ class_work1_v3.py
@@ -1,76 +1,36 @@
 import numpy as np
 import cv2 as cv
 
-# Create a black image with a white background
-image_width = 800
-image_height = 600
-background_color = (255, 255, 255)  # White color in RGB
-line_color = (0, 0, 0)  # Black color in RGB
-image = np.full((image_height, image_width, 3), background_color, dtype=np.uint8)
+# Create a blank image
+image_width = 32
+image_height = 32
+line_color = (255, 255, 255)  # White color in RGB
+image = np.zeros((image_height, image_width, 3), dtype=np.uint8)
 
 # Define the line parameters
-start_point = (100, 300)  # Starting point of the line (x, y)
-end_point = (700, 300)  # Ending point of the line (x, y)
-line_thickness = 2
+start_point = (0, 10)  # Starting point of the line (x, y)
+end_point = (20, 30)  # Ending point of the line (x, y)
+line_thickness = 1
 
-# Calculate the line slope
-slope = (end_point[1] - start_point[1]) / (end_point[0] - start_point[0])
+# Draw the line on the image
+cv.line(image, start_point, end_point, line_color, line_thickness)
 
-# Calculate the line angle
-angle = np.arctan(slope)
-
-# Calculate the line length
-line_length = int(np.sqrt((end_point[0] - start_point[0]) ** 2 + (end_point[1] - start_point[1]) ** 2))
-
-# Draw the line pixel by pixel
-for t in range(line_length):
-    x = int(start_point[0] + t * np.cos(angle))
-    y = int(start_point[1] + t * np.sin(angle))
-    image[y, x] = line_color
+# Saving the line image
+cv.imwrite("line_filter.png", image)
 
 # Load an image to convolve with the line
-image_path = "kanti.jpg" 
-original_image = cv.imread(image_path)
+image_path = "kanti.jpg"
+original_image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
 
-# Perform convolution between the line and the image
-convolved_image = cv.filter2D(original_image, -1, image)
-
-# Display the convolved image
-cv.imshow("Convolved Image", convolved_image)
-cv.waitKey(0)
-cv.destroyAllWindows()
-
-
-
-
-
-
-import cv2 as cv
-import numpy as np
-
-# Creating a blank image
-image = np.zeros((32, 32))
-
-# Drawing a line on the image
-start_point = (0, 10)
-end_point = (20, 30)
-color = (255, 255, 255)
-thickness = 1
-image = cv.line(image, start_point, end_point, color, thickness)
-
-# Saving the image
-cv.imwrite("./filter.png", image)
-
-# Reading the input image and the filter image
-input_image = cv.imread("./kanti.jpg", cv.IMREAD_GRAYSCALE)
-filter_image = cv.imread("./filter.png", cv.IMREAD_GRAYSCALE)
+# Reading the line filter image
+line_filter_image = cv.imread("line_filter.png", cv.IMREAD_GRAYSCALE)
 
 # Normalizing the filter image
-filter_sum = filter_image.sum()
-filter_image = filter_image / filter_sum
+line_filter_sum = line_filter_image.sum()
+line_filter_image = line_filter_image / line_filter_sum
 
-# Applying the filter
-output_image = cv.filter2D(src=input_image, ddepth=-1, kernel=filter_image)
+# Applying the filter (convolution)
+output_image = cv.filter2D(src=original_image, ddepth=-1, kernel=line_filter_image)
 
 # Saving the output image
-cv.imwrite("./output_image.png", output_image)
+cv.imwrite("output_image.png", output_image)
